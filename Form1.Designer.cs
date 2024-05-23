85e201fdc722bd51adf6e4b5533943cd191d632c
Umar <155890863+Hasanovumar@users.noreply.github.com>
2024-05-23 15:14:19 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtTotal = new TextBox();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn9 = new Button();
+            btn0 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            btn6 = new Button();
+            btnDiv = new Button();
+            btnClear = new Button();
+            btnMul = new Button();
+            btnMin = new Button();
+            btnPlus = new Button();
+            btnEql = new Button();
+            pictureBox1 = new PictureBox();
+            button_XAxis = new Button();
+            textBox_XAxis = new TextBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // txtTotal
+            // 
+            txtTotal.Font = new Font("Segoe UI Semibold", 24F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            txtTotal.Location = new Point(12, 12);
+            txtTotal.Multiline = true;
+            txtTotal.Name = "txtTotal";
+            txtTotal.Size = new Size(350, 71);
+            txtTotal.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            btn1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn1.Location = new Point(12, 100);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(54, 46);
+            btn1.TabIndex = 2;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += button1_Click;
+            // 
+            // btn2
+            // 
+            btn2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn2.Location = new Point(85, 100);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(50, 46);
+            btn2.TabIndex = 2;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn3
+            // 
+            btn3.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn3.Location = new Point(152, 100);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(53, 46);
+            btn3.TabIndex = 2;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btn4
+            // 
+            btn4.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn4.Location = new Point(220, 100);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(53, 46);
+            btn4.TabIndex = 2;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // btn5
+            // 
+            btn5.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn5.Location = new Point(299, 100);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(53, 46);
+            btn5.TabIndex = 2;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
+            // 
+            // btn9
+            // 
+            btn9.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn9.Location = new Point(220, 170);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(53, 46);
+            btn9.TabIndex = 3;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // btn0
+            // 
+            btn0.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn0.Location = new Point(299, 170);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(53, 46);
+            btn0.TabIndex = 4;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn0_Click;
+            // 
+            // btn8
+            // 
+            btn8.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn8.Location = new Point(152, 170);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(53, 46);
+            btn8.TabIndex = 5;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn7
+            // 
+            btn7.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn7.Location = new Point(85, 170);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(50, 46);
+            btn7.TabIndex = 6;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // btn6
+            // 
+            btn6.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn6.Location = new Point(12, 170);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(54, 46);
+            btn6.TabIndex = 7;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // btnDiv
+            // 
+            btnDiv.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnDiv.Location = new Point(220, 237);
+            btnDiv.Name = "btnDiv";
+            btnDiv.Size = new Size(53, 46);
+            btnDiv.TabIndex = 8;
+            btnDiv.Text = "/";
+            btnDiv.UseVisualStyleBackColor = true;
+            btnDiv.Click += btnDiv_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnClear.Location = new Point(289, 237);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(77, 46);
+            btnClear.TabIndex = 9;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnMul
+            // 
+            btnMul.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnMul.Location = new Point(152, 237);
+            btnMul.Name = "btnMul";
+            btnMul.Size = new Size(53, 46);
+            btnMul.TabIndex = 10;
+            btnMul.Text = "*";
+            btnMul.UseVisualStyleBackColor = true;
+            btnMul.Click += btnMul_Click;
+            // 
+            // btnMin
+            // 
+            btnMin.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnMin.Location = new Point(85, 237);
+            btnMin.Name = "btnMin";
+            btnMin.Size = new Size(50, 46);
+            btnMin.TabIndex = 11;
+            btnMin.Text = "-";
+            btnMin.UseVisualStyleBackColor = true;
+            btnMin.Click += btnMin_Click;
+            // 
+            // btnPlus
+            // 
+            btnPlus.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnPlus.Location = new Point(12, 237);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(54, 46);
+            btnPlus.TabIndex = 12;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = true;
+            btnPlus.Click += btnPlus_Click;
+            // 
+            // btnEql
+            // 
+            btnEql.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnEql.Location = new Point(12, 298);
+            btnEql.Name = "btnEql";
+            btnEql.Size = new Size(350, 46);
+            btnEql.TabIndex = 12;
+            btnEql.Text = "=";
+            btnEql.TextImageRelation = TextImageRelation.TextAboveImage;
+            btnEql.UseVisualStyleBackColor = true;
+            btnEql.Click += btnEql_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(372, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(500, 300);
+            pictureBox1.TabIndex = 13;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button_XAxis
+            // 
+            button_XAxis.Location = new Point(485, 327);
+            button_XAxis.Name = "button_XAxis";
+            button_XAxis.Size = new Size(75, 23);
+            button_XAxis.TabIndex = 14;
+            button_XAxis.Text = "X Max";
+            button_XAxis.UseVisualStyleBackColor = true;
+            button_XAxis.Click += button_XAxis_Click;
+            // 
+            // textBox_XAxis
+            // 
+            textBox_XAxis.Location = new Point(379, 327);
+            textBox_XAxis.Name = "textBox_XAxis";
+            textBox_XAxis.Size = new Size(100, 23);
+            textBox_XAxis.TabIndex = 15;
+            textBox_XAxis.TextChanged += textBox_XAxis_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(797, 330);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 16;
+            button1.Text = "Prozent";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(691, 331);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 15;
+            textBox1.TextChanged += textBox_XAxis_TextChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(566, 319);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(41, 19);
+            radioButton1.TabIndex = 17;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Sin";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(563, 341);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(45, 19);
+            radioButton2.TabIndex = 18;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Cos";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(915, 365);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(textBox_XAxis);
+            Controls.Add(button_XAxis);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnDiv);
+            Controls.Add(btnClear);
+            Controls.Add(btnMul);
+            Controls.Add(btnMin);
+            Controls.Add(btnEql);
+            Controls.Add(btnPlus);
+            Controls.Add(btn9);
+            Controls.Add(btn0);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(txtTotal);
+            Name = "Form1";
+            Text = "calculator";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtTotal;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn9;
+        private Button btn0;
+        private Button btn8;
+        private Button btn7;
+        private Button btn6;
+        private Button btnDiv;
+        private Button btnClear;
+        private Button btnMul;
+        private Button btnMin;
+        private Button btnPlus;
+        private Button btnEql;
+        private PictureBox pictureBox1;
+        private Button button_XAxis;
+        private TextBox textBox_XAxis;
+        private Button button1;
+        private TextBox textBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,366 @@
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Paint += PictureBox1_Paint;
+        }
+        public bool IsSinus { get => radioButton1.Checked; }
+        public bool IsCosinus { get => radioButton2.Checked; }
+        private void PictureBox1_Paint(object? sender, PaintEventArgs e)
+        {
+            var graphic = e.Graphics;
+            var blackPen = new Pen(Color.Black);
+            var width = pictureBox1.Width;
+            var height = pictureBox1.Height;
+            Point NullPoint = new Point(10, height / 2);
+            Point XAxisPoint = new Point(x: NullPoint.X + ((int)(XLenthProcent / 100.0 * width)), y: NullPoint.Y);
+            Point YAxisPoint = new Point(x: NullPoint.X, y: NullPoint.Y - ((int)(XLenthProcent / 2 / 100.0 * height)));
+            Point YAxisPointMinus = new Point(x: NullPoint.X, y: NullPoint.Y + ((int)(XLenthProcent / 2 / 100.0 * height)));
+
+            graphic.DrawLine(blackPen,
+                NullPoint,
+                XAxisPoint
+                );
+
+            graphic.DrawLine(blackPen,
+              NullPoint,
+              YAxisPoint
+              );
+
+            graphic.DrawLine(blackPen,
+              NullPoint,
+              YAxisPointMinus
+              );
+
+            //graphic.DrawLine(blackPen,
+            //    x1: 250, y1: 50,
+            //    x2: 250, y2: 200
+            //    );
+            //graphic.DrawLine(blackPen,
+            //    x1: 250, y1: 200,
+            //    x2: 400, y2: 200
+            //    );
+
+            //graphic.DrawLine(blackPen,
+            //    x1: 250, y1: 50,
+            //    x2: 240, y2: 60
+            //    );
+            //graphic.DrawLine(blackPen,
+            //    x1: 250, y1: 50,
+            //    x2: 260, y2: 60
+            //    );
+
+            //graphic.DrawLine(blackPen,
+            //    x1: 390, y1: 190,
+            //    x2: 400, y2: 200
+            //     );
+
+            //graphic.DrawLine(blackPen,
+            //    x1: 390, y1: 210,
+            //    x2: 400, y2: 200
+            //     );
+
+            //graphic.DrawLine(blackPen,
+            //   x1: 240, y1: 125,
+            //   x2: 260, y2: 125
+            //    );
+
+            //graphic.DrawLine(blackPen,
+            //  x1: 322, y1: 210,
+            //  x2: 322, y2: 190
+            //   );
+
+            graphic.DrawString(
+                s: "Achse Y",
+                font: Font,
+                brush: Brushes.Black,
+                YAxisPoint
+                );
+
+            graphic.DrawString(
+                s: "Achse X",
+                font: Font,
+                brush: Brushes.Black,
+                XAxisPoint
+                );
+
+
+
+            graphic.DrawString(
+                s: "0",
+                font: Font,
+                brush: Brushes.Black,
+                 NullPoint
+                );
+
+            graphic.DrawString(
+                s: "" + Ymax,
+                font: Font,
+                brush: Brushes.Black,
+                x: YAxisPoint.X,
+                y: YAxisPoint.Y + 10
+                );
+
+            graphic.DrawString(
+                s: "-" + Ymax,
+                font: Font,
+                brush: Brushes.Black,
+                x: YAxisPointMinus.X,
+                y: YAxisPointMinus.Y /*+ 10*/
+                );
+
+            graphic.DrawString(
+
+                s: "" + Xmax,
+                font: Font,
+                brush: Brushes.Black,
+                x: XAxisPoint.X,
+                y: XAxisPoint.Y + 10
+                );
+
+            graphic.DrawString(
+                s: "" + Xmax / 2,
+                font: Font,
+                brush: Brushes.Black,
+                x: NullPoint.X + (XAxisPoint.X - NullPoint.X) / 2,
+                y: NullPoint.Y
+                );
+
+            graphic.DrawString(
+                s: "" + Ymax / 2.0,
+                font: Font,
+                brush: Brushes.Black,
+                x: NullPoint.X,
+                y: YAxisPoint.Y + (NullPoint.Y - YAxisPoint.Y) / 2
+                );
+
+
+            graphic.DrawString(
+                s: "-" + Ymax / 2.0,
+                font: Font,
+                brush: Brushes.Black,
+                x: NullPoint.X,
+                y: YAxisPointMinus.Y - (YAxisPointMinus.Y - NullPoint.Y) / 2
+                );
+
+            // sinus 
+            for (int i = 0; i < Xmax * 10; i++)
+            {
+                double sd = i / 10.0;
+                double result_Math = 0.0;
+                //if (IsSinus)
+                //{
+                //    result_Math = Math.Sin(sd);
+                //}
+                //else
+                //{
+                //    result_Math = Math.Cos(sd);
+                //}
+
+                result_Math = Math.Sin(sd);
+                DrawCircle(graphic, blackPen, NullPoint, XAxisPoint, YAxisPoint, sd, result_Math);
+                result_Math = Math.Cos(sd + (Math.PI / 2));
+                DrawCircle(graphic, blackPen, NullPoint, XAxisPoint, YAxisPoint, sd, result_Math);
+            }
+
+            //graphic.DrawEllipse(blackPen, new Rectangle(x: 325 - 2, y: 128 - 2, width: 4, height: 4));
+            //var result1 = Math.Sin(0.5);
+            //graphic.DrawEllipse(blackPen, new Rectangle(x: 310 - 2, y: 141 - 2, width: 4, height: 4));
+            //var result2 = Math.Sin(0.4);
+            //graphic.DrawEllipse(blackPen, new Rectangle(x: 295, y: 154, width: 4, height: 4));
+            //var result3 = Math.Sin(0.3);
+            //graphic.DrawEllipse(blackPen, new Rectangle(x: 292, y: 180, width: 7, height: 7));
+            //var result4 = Math.Sin(0.2);
+            //graphic.DrawEllipse(blackPen, new Rectangle(x: 282, y: 190, width: 7, height: 7));
+            //var result5 = Math.Sin(0.1);
+            //graphic.DrawEllipse(blackPen, new Rectangle(x: 272, y: 200, width: 7, height: 7));
+            //var result6 = Math.Sin(0.0);
+        }
+
+        private void DrawCircle(Graphics graphic, Pen blackPen, Point NullPoint, Point XAxisPoint, Point YAxisPoint, double sd, double result_Math)
+        {
+            int yPixel = (NullPoint.Y - YAxisPoint.Y) / Ymax;
+            int resultPixel = (int)(result_Math * yPixel);
+
+            int xPixel = (XAxisPoint.X - NullPoint.X) / Xmax;
+            int forOffset = (int)(sd * xPixel);
+            graphic.DrawEllipse(
+                blackPen,
+                new Rectangle(
+                    x: NullPoint.X + forOffset - 2,
+                    y: NullPoint.Y - resultPixel - 2,
+                    width: 4,
+                    height: 4));
+        }
+
+        string CalTotal;
+        int num1;
+        int num2;
+        string option;
+        int result;
+
+        int Xmax = 6;
+        int Ymax = 1;
+        int XLenthProcent = 50;
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + "9";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = txtTotal.Text + "0";
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            option = "+";
+            num1 = int.Parse(txtTotal.Text);
+
+            txtTotal.Clear();
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            option = "-";
+            num1 = int.Parse(txtTotal.Text);
+
+            txtTotal.Clear();
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            option = "*";
+            num1 = int.Parse(txtTotal.Text);
+
+            txtTotal.Clear();
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            option = "/";
+            num1 = int.Parse(txtTotal.Text);
+
+            txtTotal.Clear();
+        }
+
+        private void btnEql_Click(object sender, EventArgs e)
+        {
+            num2 = int.Parse(txtTotal.Text);
+
+            if (option.Equals("+"))
+                result = num1 + num2;
+
+            if (option.Equals("-"))
+                result = num1 - num2;
+
+            if (option.Equals("*"))
+                result = num1 * num2;
+
+            if (option.Equals("/"))
+                result = num1 / num2;
+
+            txtTotal.Text = result + "";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtTotal.Clear();
+            result = (0);
+            num1 = (0);
+            num2 = (0);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_XAxis_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBox_XAxis.Text, out int maxXValue))
+            {
+                Xmax = maxXValue;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void textBox_XAxis_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBox1.Text, out int maxXValue))
+            {
+                XLenthProcent = maxXValue;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton2.Checked = !radioButton1.Checked;
+            pictureBox1.Invalidate();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.Checked = !radioButton2.Checked;
+            pictureBox1.Invalidate();
+        }
+    }
+}
+  
